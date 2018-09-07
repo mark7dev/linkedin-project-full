0ff84626c349111604e54db0d52c819880d9f2da
mark7dev <mcamacho_ce7@live.com.mx>
2018-09-07 16:51:22 -0500

    Implement 404 and 500 handlers

--- app.js
+++ app.js
@@ -26,6 +26,75 @@ app.get('/', (request, response) => {
   response.sendFile(indexFile);
 });
 
+
+//Error 404
+/**
+ * [4] 404 Not Found
+ * Catch the error.
+ *
+ * `app.use` it’s called every time a request is sent to the
+ * server.
+ */
+app.use((request, response) => {
+  const ERROR = {
+    message: '404. Not Found.'
+  };
+
+  response.json(ERROR)
+  .status(404);
+});
+// OTHER WAY
+// app.use((request, response, next) => {
+//   const ERROR_404 = {
+//     error: {
+//       message: 'The requested resource is not defined.',
+//       status: 404
+//     }
+//   };
+
+//   next(ERROR_404);
+// });
+
+
+
+/**
+ * [5] 500 Internal Error Server
+ * Catch the error.
+ *
+ * `app.use` it’s called every time a request is sent to the
+ * server.
+ */
+app.use((error, request, response, next) => {
+  const body = error.error;
+  const STATUS_CODE = body.status || 500;
+  const ERROR_505 = body.message || '500. Internal Server Error :(';
+
+  const formatedMessage = JSON.stringify(error, null, 2);
+
+  response
+    .status(STATUS_CODE)
+    .json({
+      error: {
+        message: ERROR_505,
+        status: STATUS_CODE
+      }
+    });
+
+  console.log(chalk.red(formatedMessage));
+});
+
+
+// OTHER WAY
+// app.use((request, response) => {
+//   const ERROR = {
+//     message: '500. Internal problem.'
+//   };
+
+//   response.json(ERROR)
+//   .status(500);
+// });
+
+
 /**
  * Run and listen the server on an specific port.
  */
