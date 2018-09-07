c6b89ee0f7efa802f4dd8ed29ba362339892e985
mark7dev <mcamacho_ce7@live.com.mx>
2018-09-07 12:40:07 -0500

    Implement an environment variable to take the PORT number, if it exists

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const express = require('express');
 /* Absolute Path to HTML file. */
 const indexFile = `${ __dirname }/index.html`;
 
+
 /**
  * [1] Create and instantiate the Node server.
  * [2] The callback function will act like a Listener, it will execute each time
  *     a request is done.
  */
 const app = express();
+
+// Change PORT to 8000 (jason) if this port is occuped then run on 3000
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (request, response) => {
   response.sendFile(indexFile);
@@ -20,8 +24,8 @@ app.get('/', (request, response) => {
  * Run and listen the server on an specific port.
  */
 
-app.listen(3000, () => {
-  const formatedMessage = chalk.green('Express server running on PORT: 3000')
+app.listen(PORT, () => {
+  const formatedMessage = chalk.green(`Node server running on PORT: ${ PORT }`);
 
   console.log(formatedMessage);
 });
